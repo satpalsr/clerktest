90385b0814d34635d892739540b5680776ee3b5f
Satpal Singh Rathore <satpal.code@gmail.com>
2023-07-15 15:05:31 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
+export const runtime = 'edge'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
+    </ClerkProvider>
   )
 }
