55b6b8fb752096471027d65722fff8cccdc45123
Satpal Singh Rathore <satpal.code@gmail.com>
2023-07-15 15:31:21 +0530

    add router

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 export const runtime = 'edge'
 import { SignOutButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { useState } from 'react';
 
 export default function HomePage() {
 const [signOutText, setSignOutText] = useState('Sign Out');
 
+const router = useRouter();
+
 const handleSignOut = async () => {
       setSignOutText('Signing Out');
+      await router.push('/');
 };
 
 return  (
